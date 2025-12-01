d6427df1af02594f9fec54b8f6e1766be82bb54e
Phaolap Kulteera <pholapcondo11@gmail.com>
2025-12-01 15:20:38 +0700

    Remove logging

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -160,7 +160,6 @@ const Cancle = async (item)=>{
 }
 const SearchItem = async (block)=>{
     try{
-        console.log(block);
         const {object,total} = await getProductBySearching(block,nowPage.value,limit.value)
         filter.value = block
         pd.value = object
